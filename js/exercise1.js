fb352526d1f3eba6203f623b24e81979e3aeb5e1
Julieta Correa <mjcorrea210@gmail.com>
2023-03-12 20:59:31 -0300

    first exercise

---
+++ js/exercise1.js
@@ -0,0 +1,9 @@
+let edad = parseInt(prompt("Ingrese su edad"))
+
+console.log(typeof (edad));
+console.log(edad);
+
+if (edad >=18){
+    document.write("Puede conducir");}
+else{
+    document.write("Numero no valido");}
