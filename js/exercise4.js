8ff8dd0629204e36e88c818fa001c26d418bd2a6
Julieta Correa <mjcorrea210@gmail.com>
2023-03-12 23:37:06 -0300

    exercise4

---
+++ js/exercise4.js
@@ -0,0 +1,26 @@
+let numeros = [];
+let suma = 0;
+
+while (true) {
+  let input = prompt("Introduce un número:");
+
+  
+  if (input === null) {
+    break;
+  }
+
+  
+  if (isNaN(input)) {
+    alert("Por favor, introduce un valor numérico válido.");
+    continue;
+  }
+
+  
+  let num = Number(input);
+  numeros.push(num);
+  suma += num;
+}
+
+console.log("Los números introducidos son:", numeros);
+console.log("La suma de los números es:", suma);
+
